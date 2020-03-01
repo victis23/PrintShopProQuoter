1d37c97845a87746c7700ad20731023e78c57bf9
Michael Wells <wow@duhmarket.com>
2020-03-01 07:06:24 -0500

    Add file and struct for contact.

---
+++ PrintShopProQuoter/Models/Contact.swift
@@ -0,0 +1,13 @@
+//
+//  Contact.swift
+//  PrintShopProQuoter
+//
+//  Created by Scott Leonard on 3/1/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import Foundation
+
+struct Contact {
+	var name : String
+}
