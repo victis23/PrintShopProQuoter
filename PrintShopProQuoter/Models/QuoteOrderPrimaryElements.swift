a7ecfa684668a6e96167c00bff00b21d6ceaca6b
Michael Wells <wow@duhmarket.com>
2020-03-01 07:09:19 -0500

    Add file and structs for constructing orders and quotes.

---
+++ PrintShopProQuoter/Models/QuoteOrderPrimaryElements.swift
@@ -0,0 +1,33 @@
+//
+//  QuoteOrderPrimaryElements.swift
+//  PrintShopProQuoter
+//
+//  Created by Scott Leonard on 3/1/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import Foundation
+
+enum Catagory {
+	case menu
+	case envelope
+	case flyer
+	case graphicDesign
+}
+
+enum StockColors {
+	case white
+}
+
+enum StockWeight {
+	case _80Gloss
+}
+
+enum StockFinish {
+	case bond
+	case glossy
+}
+
+enum StockSize {
+	
+}
