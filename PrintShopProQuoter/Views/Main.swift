71aa180969786b64d858369cb345c6f55458b1ce
Michael Wells <wow@duhmarket.com>
2020-03-01 10:12:09 -0500

    import coredata framework — Add static function to retrieve data from coredata.

--- PrintShopProQuoter/Views/Main.swift
+++ PrintShopProQuoter/Views/Main.swift
@@ -8,8 +8,24 @@
 
 import UIKit
 import SwiftUI
+import CoreData
 
 class MainVC : UIHostingController<Main> {
+	
+	static func fetchCompanies() -> [CoreCompany]? {
+		
+		let context = (UIApplication.shared.delegate as! AppDelegate)
+			.persistentContainer.viewContext
+		
+		let request : NSFetchRequest<CoreCompany> = NSFetchRequest(entityName: "CoreCompany")
+		request.sortDescriptors = [NSSortDescriptor(keyPath: \CoreCompany.name, ascending: true)]
+		
+		let response = try? context.fetch(request)
+		
+		return response
+	}
+	
+	 
 
 	override init?(coder aDecoder: NSCoder, rootView: Main) {
 		super.init(coder: aDecoder, rootView: rootView)
