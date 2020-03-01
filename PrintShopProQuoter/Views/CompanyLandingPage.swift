2c49c7c0d26f72fcedb212230d7657036559332a
Michael Wells <wow@duhmarket.com>
2020-03-01 11:44:45 -0500

    Add buttons to view.

--- PrintShopProQuoter/Views/CompanyLandingPage.swift
+++ PrintShopProQuoter/Views/CompanyLandingPage.swift
@@ -12,10 +12,46 @@ struct CompanyLandingPage : View {
 	var company : Company?
 	
 	var body: some View {
-		Text(company?.name ?? "No EnteredValue")
+		NavigationView{
+			HStack {
+				VStack {
+					ItemButton(company: company, text: "Quotes")
+					ItemButton(company: company, text: "Orders")
+					Spacer()
+				}
+				VStack {
+					ItemButton(company: company, text: "Contact Info")
+					ItemButton(company: company, text: "Location")
+					Spacer()
+				}
+			}
+		.padding()
+		}
+		.navigationBarTitle(company?.name ?? "No Company")
 	}
 }
 
+struct ItemButton: View {
+	
+	@State var isPresenting :Bool = false
+	var company : Company?
+	
+	var text : String
+	var body: some View {
+		Button(action: {
+			self.isPresenting = true
+		}){
+			ZStack {
+				RoundedRectangle(cornerRadius: 10)
+					.aspectRatio(contentMode: .fit)
+					.foregroundColor(Color(.secondarySystemFill))
+				Text(text)
+					.foregroundColor(Color(.label))
+					.bold()
+			}
+		}
+	}
+}
 struct Preview : PreviewProvider {
 	
 	static var previews: some View {
