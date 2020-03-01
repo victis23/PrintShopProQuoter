006e8f60de7756f421451b1e7a711b0468f68b9a
Michael Wells <wow@duhmarket.com>
2020-03-01 07:07:01 -0500

    Add file and struct for Address

---
+++ PrintShopProQuoter/Models/Address.swift
@@ -0,0 +1,18 @@
+//
+//  Address.swift
+//  PrintShopProQuoter
+//
+//  Created by Scott Leonard on 3/1/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import Foundation
+
+struct Address {
+	
+	var street : String
+	var city : String?
+	var state : String?
+	var country : String?
+	var zipcode : String?
+}
