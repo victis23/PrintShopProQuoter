6f736aed09a3965a0ac7a3c17805bdf411e49a4e
Michael Wells <wow@duhmarket.com>
2020-03-01 19:41:06 -0500

    Extract helper class remove import frameworks

--- PrintShopProQuoter/Controllers/MainController.swift
+++ PrintShopProQuoter/Controllers/MainController.swift
@@ -6,31 +6,16 @@
 //  Copyright © 2020 DuhMarket. All rights reserved.
 //
 
-import UIKit
+
 import SwiftUI
-import CoreData
 
 class MainVC : UIHostingController<Main> {
-	
-	static func fetchCompanies() -> [CoreCompany]? {
-		
-		let context = (UIApplication.shared.delegate as! AppDelegate)
-			.persistentContainer.viewContext
-		
-		let request : NSFetchRequest<CoreCompany> = NSFetchRequest(entityName: "CoreCompany")
-		request.sortDescriptors = [NSSortDescriptor(keyPath: \CoreCompany.name, ascending: true)]
-		
-		let response = try? context.fetch(request)
-		
-		return response
-	}
 	
 	override init?(coder aDecoder: NSCoder, rootView: Main) {
 		super.init(coder: aDecoder, rootView: rootView)
 	}
 	
 	@objc required dynamic init?(coder aDecoder: NSCoder) {
-		
-		super.init(coder: aDecoder, rootView: Main(displayText: "This is only a test!", retrievedList: MainVC.fetchCompanies()))
+		super.init(coder: aDecoder, rootView: Main(retrievedList: CompanyFetcher().fetchFromCoreData()))
 	}
 }
