27cb2eef2a2e1e40e706c844d27ac3276116070a
Michael Wells <wow@duhmarket.com>
2020-03-01 19:39:07 -0500

    Extract helperClass for retrieving items from persistent container.

---
+++ PrintShopProQuoter/HelperClasses/File.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  PrintShopProQuoter
+//
+//  Created by Scott Leonard on 3/1/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+/// FetchCompanies retrieves data from persistent container and returns an array of CoreCompany objects.
+class CompanyFetcher {
+	
+	private var context : NSManagedObjectContext {
+		let context = (UIApplication.shared.delegate as! AppDelegate)
+			.persistentContainer.viewContext
+		return context
+	}
+	
+	private let request : NSFetchRequest<CoreCompany> = NSFetchRequest(entityName: "CoreCompany")
+	
+	
+	func fetchFromCoreData() -> [CoreCompany]? {
+		request.sortDescriptors = [NSSortDescriptor(keyPath: \CoreCompany.name, ascending: true)]
+		let response = try? context.fetch(request)
+		return response
+	}
+}
