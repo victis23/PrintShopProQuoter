3f9f10946d51a9e2b1d984e09641d47730d6c4e4
Michael Wells <wow@duhmarket.com>
2020-03-02 10:53:43 -0500

    Change background and foreground colors.

--- PrintShopProQuoter/Views/AddCompany.swift
+++ PrintShopProQuoter/Views/AddCompany.swift
@@ -23,60 +23,59 @@ struct AddCompany: View {
 	var body: some View {
 		
 		NavigationView {
-			VStack{
-			List(content: {
-				
-				TextField("Company Name", text: $name)
-					.foregroundColor(Color(UIColor.label))
-					.padding()
-				
-				NavigationLink(destination: AddAddress(), label: {
-					Text("Address")
-						.foregroundColor(Color(.link))
-						.padding(.all, 20)
-				})
-				
-				
-				NavigationLink(destination: AddContact(), label: {
-					Text("Contact Info")
-						.foregroundColor(Color(.link))
-						.padding(.all, 20)
-				})
-			})
-			
-			Spacer()
-			
-			Button(action: {
-				let newCompany = Company(name: self.name, address: Address(street: self.address), contact: Contact(name: self.contact), quotes: nil, orders: nil)
-				self.customerList.companies.append(newCompany)
-				
-			
-				let company = CoreCompany(context: self.context)
-					company.name = newCompany.name
-					company.id = newCompany.id
-				let address = CoreAddress(context: self.context)
-				address.street = newCompany.address.street
-				company.companyAddress?.street = address.street
-				try? self.context.save()
-				
-				
-				self.presentationMode.wrappedValue.dismiss()
-			}, label: {
-				ZStack {
-					Capsule()
-						.frame(minWidth: 250, idealWidth: 265, maxWidth: 350, minHeight: 50, idealHeight: 55, maxHeight: 60, alignment: .center)
-					
-					Text("Save New Company")
-						.font(.largeTitle)
+			ZStack{
+				GradientBackground()
+				VStack{
+					List(content: {
+						
+						TextField("Company Name", text: $name)
+							.padding()
+						
+						NavigationLink(destination: AddAddress(), label: {
+							Text("Address")
+								.padding(.all, 20)
+						})
+						
+						NavigationLink(destination: AddContact(), label: {
+							Text("Contact Info")
+								.padding(.all, 20)
+						})
+					})
 						.foregroundColor(.white)
-						.bold()
-						.padding(.horizontal, 10)
+					Spacer()
 					
+					Button(action: {
+						let newCompany = Company(name: self.name, address: Address(street: self.address), contact: Contact(name: self.contact), quotes: nil, orders: nil)
+						self.customerList.companies.append(newCompany)
+						
+						
+						let company = CoreCompany(context: self.context)
+						company.name = newCompany.name
+						company.id = newCompany.id
+						let address = CoreAddress(context: self.context)
+						address.street = newCompany.address.street
+						company.companyAddress?.street = address.street
+						try? self.context.save()
+						
+						
+						self.presentationMode.wrappedValue.dismiss()
+					}, label: {
+						ZStack {
+							Capsule()
+								.frame(minWidth: 250, idealWidth: 265, maxWidth: 350, minHeight: 50, idealHeight: 55, maxHeight: 60, alignment: .center)
+								.foregroundColor(.white)
+							Text("Save New Company")
+								.font(.largeTitle)
+								.foregroundColor(Color(.link))
+								.bold()
+								.padding(.horizontal, 10)
+							
+						}
+					})
+						.padding()
 				}
-			})
-				.padding()
 			}
-		.navigationBarTitle("Add Company")
+			.navigationBarTitle("Add Company")
 		}
 	}
 }
