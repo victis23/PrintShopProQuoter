3323a6d4fce650a4fca4f191903a513980df399b
Michael Wells <wow@duhmarket.com>
2020-03-04 19:07:36 -0500

    Add struct for Adding Contacts to view.

--- PrintShopProQuoter/Views/AddContact.swift
+++ PrintShopProQuoter/Views/AddContact.swift
@@ -9,13 +9,56 @@
 import SwiftUI
 
 struct AddContact: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	
+	@Environment(\.presentationMode) private var presentationMode
+	@Binding private var contact : Contact
+	@State private var name : String = ""
+	@State private var phoneNumber : String = ""
+	@State private var email : String = ""
+	
+	init(contact: Binding<Contact>){
+		self._contact = contact
+	}
+	
+	var body: some View {
+		
+		ZStack{
+			
+			GradientBackground()
+			
+			VStack{
+				
+				Form{
+					
+					TextField("Name", text: $name)
+					.padding()
+					
+					TextField("Phone Number", text: $phoneNumber)
+					.padding()
+					
+					TextField("Email", text: $email)
+					.padding()
+				}
+				.foregroundColor(.white)
+				
+				Button(action: {
+					self.contact.name = self.name
+					self.contact.phone = self.phoneNumber
+					self.contact.email = self.email
+					self.presentationMode.wrappedValue.dismiss()
+					
+				}) {
+					
+					Text("Submit")
+						.foregroundColor(.white)
+				}
+			}
+		}
+	}
 }
 
 struct AddContact_Previews: PreviewProvider {
-    static var previews: some View {
-        AddContact()
-    }
+	static var previews: some View {
+		AddContact(contact: .constant(Contact()))
+	}
 }
