adb3dc5fc2af04701a50cd62ba10e821a5dc32d1
Michael Wells <wow@duhmarket.com>
2020-03-04 20:49:49 -0500

    reordering into logical order.

--- PrintShopProQuoter/Controllers/MainVC.swift
+++ PrintShopProQuoter/Controllers/MainVC.swift
@@ -9,14 +9,16 @@
 
 import SwiftUI
 
+//Hosts a view that will contain a struct class that conforms to swiftUI's View protocol.
 class MainVC : UIHostingController<Main> {
 	
-	override init?(coder aDecoder: NSCoder, rootView: Main) {
-		super.init(coder: aDecoder, rootView: rootView)
-	}
-	
-	///Uses array of Coredata objects to instantiate `Main`.
+	///1. — Uses array of Coredata objects to instantiate `Main`.
 	@objc required dynamic init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder, rootView: Main(retrievedList: CompanyFetcher().fetchFromCoreData()))
+	}
+	
+	///2. — Called by `@objc required dynamic init?` to construct view.
+	override init?(coder aDecoder: NSCoder, rootView: Main) {
+		super.init(coder: aDecoder, rootView: rootView)
 	}
 }
