aad36f1e9e0fe09f4874a0f7be9ac532f26b3c4a
Michael Wells <wow@duhmarket.com>
2020-03-06 00:24:10 -0500

    Add key path as argument to call.

--- PrintShopProQuoter/Controllers/MainVC.swift
+++ PrintShopProQuoter/Controllers/MainVC.swift
@@ -14,7 +14,7 @@ class MainVC : UIHostingController<Main> {
 	
 	///1. — Uses array of Coredata objects to instantiate `Main`.
 	@objc required dynamic init?(coder aDecoder: NSCoder) {
-		super.init(coder: aDecoder, rootView: Main(retrievedList: CompanyFetcher().fetchFromCoreData()))
+		super.init(coder: aDecoder, rootView: Main(retrievedList: CompanyFetcher(keypath: \CoreCompany.name).fetchFromCoreData()))
 	}
 	
 	///2. — Called by `@objc required dynamic init?` to construct view.
