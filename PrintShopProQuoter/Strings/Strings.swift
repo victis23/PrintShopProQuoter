c53e13ded46455377bc8f564d5ff9ecadb49fa00
Michael Wells <wow@duhmarket.com>
2020-03-07 21:10:23 -0500

    Create files for strings.

---
+++ PrintShopProQuoter/Strings/Strings.swift
@@ -0,0 +1,15 @@
+//
+//  Strings.swift
+//  PrintShopProQuoter
+//
+//  Created by Scott Leonard on 3/7/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import Foundation
+
+let LOCATION = "Location"
+let QUOTES = "Quotes"
+let ORDERS = "Orders"
+let CONTACT_INFO = "Contact Info"
+let NO_COMPANY_DEFAULT_STRING = "No Company"
