9c9193027c50ede08d2d4dacc5dc18289bb819cf
Michael Wells <wow@duhmarket.com>
2020-03-09 15:19:32 -0400

    Add Preview

--- PrintShopProQuoter/Views/CompanyLandingPage/ButtonIcon.swift
+++ PrintShopProQuoter/Views/CompanyLandingPage/ButtonIcon.swift
@@ -19,3 +19,16 @@ struct ButtonIcon: View {
 			.frame(alignment: .center)
 	}
 }
+
+struct ButtonIcon_Preview : PreviewProvider {
+	
+	static var previews: some View {
+		VStack{
+			ButtonIcon(text: "Orders")
+			ButtonIcon(text: "Quotes")
+			ButtonIcon(text: "Location")
+			ButtonIcon(text: "Contact Info")
+		}
+		.previewLayout(.sizeThatFits)
+	}
+}
