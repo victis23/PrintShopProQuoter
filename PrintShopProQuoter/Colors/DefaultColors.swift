9438d196c389c36019384ccbe2813cb04ef2db2e
Michael Wells <wow@duhmarket.com>
2020-03-10 13:14:50 -0400

    Create file with default color hues

---
+++ PrintShopProQuoter/Colors/DefaultColors.swift
@@ -0,0 +1,12 @@
+//
+//  DefaultColors.swift
+//  PrintShopProQuoter
+//
+//  Created by Scott Leonard on 3/10/20.
+//  Copyright © 2020 DuhMarket. All rights reserved.
+//
+
+import UIKit
+
+let LightBlueHue_DEFAULT = UIColor(red: 0.25, green: 0.81, blue: 1, alpha: 0.8).cgColor
+let DarkBlueHue_DEFAULT = UIColor(red: 0.0, green: 0.39, blue: 0.95, alpha: 1).cgColor
