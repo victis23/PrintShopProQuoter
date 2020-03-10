d3a28ab1abdb96b347e3740ea05c1016d2fe52e3
Michael Wells <wow@duhmarket.com>
2020-03-10 13:15:53 -0400

    Add gradient background to view.

--- PrintShopProQuoter/Controllers/OrdersViewController.swift
+++ PrintShopProQuoter/Controllers/OrdersViewController.swift
@@ -11,9 +11,41 @@ import UIKit
 class OrdersViewController: UIViewController, AccessControllerProtocol {
 	
 	var company : Company!
-	//	@IBOutlet var companyNameLabel : UILabel!
+	@IBOutlet var companyNameLabel : UILabel!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		setBackgroundColor()
+		companyNameLabel.text = company.name
+	}
+	
+	func setBackgroundColor(){
+		
+		let backgroundGradient = BackgroundGradient()
+		let viewWithGradient = backgroundGradient.setGradientToView(with: [DarkBlueHue_DEFAULT,LightBlueHue_DEFAULT], size: view.bounds)
+		
+		view.addSubview(viewWithGradient)
+		view.sendSubviewToBack(viewWithGradient)
+	}
+}
+
+class BackgroundGradient {
+	
+	private func createView()-> UIView {
+		let view = UIView()
+		
+		return view
+	}
+	
+	func setGradientToView(with colors: [CGColor], size : CGRect) -> UIView {
+		let gradient = CAGradientLayer()
+		gradient.type = .axial
+		gradient.colors = colors
+		gradient.startPoint = CGPoint(x: 1, y: 0)
+		gradient.endPoint = CGPoint(x: 1, y: 1)
+		let view = createView()
+		gradient.frame = size
+		view.layer.addSublayer(gradient)
+		return view
 	}
 }
