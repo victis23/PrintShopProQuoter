ac396837b47a1525a45d2573fdff9a6b4e6492d5
Michael Wells <wow@duhmarket.com>
2020-03-18 21:29:36 -0400

    Documentation.

--- PrintShopProQuoterTests/PrintShopProQuoterTests.swift
+++ PrintShopProQuoterTests/PrintShopProQuoterTests.swift
@@ -22,7 +22,7 @@ class PrintShopProQuoterTests: XCTestCase {
         fetcher = nil
     }
 
-	/// Tests whether or not fetch method is returning values from persistant container.
+	/// Tests whether or not fetch method is returning values from persistant container. If method should return Array<CoreCompany?>.
 	func testFetcherClass(){
 		XCTAssertNotNil(fetcher.fetchFromCoreData())
 	}
