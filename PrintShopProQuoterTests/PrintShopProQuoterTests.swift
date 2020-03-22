094f66e89ba84ee5093de235bb4e3e3f370bf2aa
Michael Wells <wow@duhmarket.com>
2020-03-22 10:32:24 -0400

    Create test for Saver Class.

--- PrintShopProQuoterTests/PrintShopProQuoterTests.swift
+++ PrintShopProQuoterTests/PrintShopProQuoterTests.swift
@@ -26,5 +26,31 @@ class PrintShopProQuoterTests: XCTestCase {
 	func testFetcherClass(){
 		XCTAssertNotNil(fetcher.fetchFromCoreData())
 	}
+	
+	func testContext() {
+		XCTAssertNotNil(Saver.context)
+	}
+	
+	func testSaverClass(){
+		
+		saver.set()
+		
+		let id = "test"
+		
+		let _localFetcher = Fetcher<CoreCompany, String?, CoreCompany>(sortBy: \CoreCompany.id, request: NSFetchRequest(entityName: CORE_COMPANY))
+		
+		let savedCompany = _localFetcher.fetchFromCoreData(predicate: NSPredicate(format: "id = %@", id))
+		XCTAssertTrue(savedCompany?[0].id == id)
+		
+		// Erase test value from Coredata as part of our cleanup.
+		if !(savedCompany?.isEmpty ?? false)  {
+			deleter(id: id)
+		}
+	}
+	
+	func deleter(id:String){
+		let deleter = Deleter<NSFetchRequestResult, CoreCompany, String?>(identifier: id, keypath: \CoreCompany.name, CDType: NSFetchRequest(entityName: CORE_COMPANY), filter: "id = %@")
+		try? deleter?.removeObject()
+	}
 
 }
