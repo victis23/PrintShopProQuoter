1c904f4fd96f1a32f76a9fb4b16afd04931591ca
Michael Wells <wow@duhmarket.com>
2020-03-22 17:56:07 -0400

    Extract functionality from testAddressButtonActivation an encapsulate in UITestingSupportClass.

--- PrintShopProQuoterUITests/PrintShopProQuoterUITests.swift
+++ PrintShopProQuoterUITests/PrintShopProQuoterUITests.swift
@@ -9,33 +9,76 @@
 import XCTest
 
 class PrintShopProQuoterUITests: XCTestCase {
+	
+	var app : XCUIApplication!
+	var uiTestHelper : UITestingSupportClass!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+		
+		app = XCUIApplication()
+		uiTestHelper = UITestingSupportClass(withQueryElement: app.tables)
+		
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+		app = nil
+		uiTestHelper = nil
     }
 
     func testAddAddressButtonActivation() throws {
         
-		let app = XCUIApplication()
 		app.launch()
 		app.navigationBars["Customer List"].buttons["Add Customer"].tap()
 		
-		let tablesQuery = app.tables
+		//Sets Company Name in Company Name Field.
+		uiTestHelper.setupCompanyName()
+		
+		//Transition to Address Input Screen.
+		uiTestHelper.setupAddress()
+		
+		let trayAndArrowDownFillButton = app.buttons["tray.and.arrow.down.fill"]
+		
+		XCTAssertTrue(trayAndArrowDownFillButton.isEnabled)
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+	
+	func testRemoveListMemeber() throws {
+		
+		app.launch()
+		
+	}
+}
+
+
+class UITestingSupportClass {
+	
+	private var tablesQuery : XCUIElementQuery
+	
+	init(withQueryElement tablesQuery: XCUIElementQuery){
+		self.tablesQuery = tablesQuery
+	}
+	
+	func getTableQuery()-> XCUIElementQuery {
+		return tablesQuery
+	}
+	
+	func setupCompanyName(){
 		
 		let companyField = tablesQuery/*@START_MENU_TOKEN@*/.textFields["Company Name"]/*[[".cells.textFields[\"Company Name\"]",".textFields[\"Company Name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
 		companyField.tap()
 		companyField.typeText("TestCompany")
-		
-		//Transition to Address Input Screen.
+	}
+	
+	func setupAddress() {
 		
 		tablesQuery/*@START_MENU_TOKEN@*/.buttons["Address\n ,  "]/*[[".cells.buttons[\"Address\\n ,  \"]",".buttons[\"Address\\n ,  \"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
 		
@@ -59,18 +102,6 @@ class PrintShopProQuoterUITests: XCTestCase {
 		
 		zipcodeField.tap()
 		zipcodeField.typeText("test zipcode")
-		
-		let trayAndArrowDownFillButton = app.buttons["tray.and.arrow.down.fill"]
-		
-		XCTAssertTrue(trayAndArrowDownFillButton.isEnabled)
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+	}
+	
 }
