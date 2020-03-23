72bf874b23a9968eb5e6170a4af5e536bbcc732f
Michael Wells <wow@duhmarket.com>
2020-03-23 07:55:30 -0400

    Create test that checks the whole CRM is functioning.

--- PrintShopProQuoterUITests/PrintShopProQuoterUITests.swift
+++ PrintShopProQuoterUITests/PrintShopProQuoterUITests.swift
@@ -12,23 +12,30 @@ class PrintShopProQuoterUITests: XCTestCase {
 	
 	var app : XCUIApplication!
 	var uiTestHelper : UITestingSupportClass!
-
-    override func setUpWithError() throws {
+	
+	override func setUpWithError() throws {
 		
 		app = XCUIApplication()
 		uiTestHelper = UITestingSupportClass(withQueryElement: app.tables, on: app)
 		
-        continueAfterFailure = false
-    }
-
-    override func tearDownWithError() throws {
+		continueAfterFailure = false
+	}
+	
+	override func tearDownWithError() throws {
 		app = nil
 		uiTestHelper = nil
-    }
-
+	}
+	
+	
+	func testAddCompanyName() throws {
+		app.launch()
+		app.navigationBars["Customer List"].buttons["Add Customer"].tap()
+		uiTestHelper.setupCompanyName()
+	}
+	
 	/// Checks that submit button becomes active once all fields are filled on the form presented by the AddCustomer view.
-    func testAddAddressButtonActivation() throws {
-        
+	func testAddAddressButtonActivation() throws {
+		
 		app.launch()
 		app.navigationBars["Customer List"].buttons["Add Customer"].tap()
 		
@@ -41,16 +48,16 @@ class PrintShopProQuoterUITests: XCTestCase {
 		let trayAndArrowDownFillButton = app.buttons["tray.and.arrow.down.fill"]
 		
 		XCTAssertTrue(trayAndArrowDownFillButton.isEnabled)
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+	}
+	
+	func testLaunchPerformance() throws {
+		if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+			// This measures how long it takes to launch your application.
+			measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+				XCUIApplication().launch()
+			}
+		}
+	}
 	
 	/// For user to submit a contact email address must be valid. This test checks that validation is functioning properly when email address is valid.
 	func testAddContactWithValidEmail() throws {
@@ -91,6 +98,29 @@ class PrintShopProQuoterUITests: XCTestCase {
 		let submitContact = app.buttons["tray.and.arrow.down.fill"]
 		
 		XCTAssertTrue(submitContact.isEnabled)
+	}
+	
+	/// Verify that user CRM is working by combining all of the tasks until user returns to main screen.
+	
+	func testCRM() throws {
+		
+		app.launch()
+		app.navigationBars["Customer List"].buttons["Add Customer"].tap()
+		uiTestHelper.setupCompanyName()
+		uiTestHelper.setupAddress()
+		app.buttons["tray.and.arrow.down.fill"].tap()
+		uiTestHelper.setupContactWithValidEmail()
+		app.buttons["pencil.and.ellipsis.rectangle"].tap()
+		app.buttons["tray.and.arrow.down.fill"].tap()
+		app.buttons["tray.and.arrow.down.fill"].tap()
+		
+		//		XCTAssertTrue(app.navigationBars["Customer List"].buttons["Add Customer"].isHittable)
+		
+		//Clean up after test.
+		//		uiTestHelper.getTableQuery()/*@START_MENU_TOKEN@*/.buttons["Address Test\nTest\n0\nQuotes\n0\nOrders"]/*[[".cells.buttons[\"Address Test\\nTest\\n0\\nQuotes\\n0\\nOrders\"]",".buttons[\"Address Test\\nTest\\n0\\nQuotes\\n0\\nOrders\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeLeft()
+		//		uiTestHelper.getTableQuery()/*@START_MENU_TOKEN@*/.buttons["trailing0"]/*[[".cells",".buttons[\"Delete\"]",".buttons[\"trailing0\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+		
+		
 	}
 }
 
